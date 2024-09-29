da1cc4de89b340cc546addc195901d9b614c1e29
unknown <shababhasnat@gmail.com>
2024-09-28 21:51:50 -0600

    search and filter functinoality added

---
+++ data/index.ts
@@ -0,0 +1,43 @@
+export const dogCards = [
+    {
+      title: "Ben",
+      breed: "Shepherd",
+      description: "Loyal and highly intelligent breed.",
+      src: "/cool.png",
+      ctaText: "Adopt",
+      ctaLink: "https://www.dogfinder.com/shepherds",
+      content: "Some more detailed information about the breed.",
+    },
+    {
+      title: "Fin",
+      breed: "Retriever",
+      description: "Friendly and great with children.",
+      src: "/cool.png",
+      ctaText: "Adopt",
+      ctaLink: "https://www.dogfinder.com/retrievers",
+      content: "Some more detailed information about the breed.",
+    },
+    
+  ];
+
+  export const catCards = [
+    {
+      title: "Lily",
+      breed: "shorthair",
+      description: "Loyal and highly intelligent breed.",
+      src: "/cool.png",
+      ctaText: "Adopt",
+      ctaLink: "https://www.dogfinder.com/shepherds",
+      content: "Some more detailed information about the breed.",
+    },
+    {
+      title: "Mewtwo",
+      breed: "orange",
+      description: "Friendly and great with children.",
+      src: "/cool.png",
+      ctaText: "Adopt",
+      ctaLink: "https://www.dogfinder.com/retrievers",
+      content: "Some more detailed information about the breed.",
+    },
+    
+  ];
